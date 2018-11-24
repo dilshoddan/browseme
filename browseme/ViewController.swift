98dae4ec2fd54ce1b268fbe38efa990143c5174c
Admin <admin@mac-admin.local>
2018-11-24 10:40:31 +0300

    Initial Commit

---
+++ browseme/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  browseme
+//
+//  Created by Admin on 11/24/18.
+//  Copyright © 2018 Admin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
